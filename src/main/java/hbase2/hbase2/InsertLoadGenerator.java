d2de03ffe490fdb4f3a1719b7fe9fc19e0148e56
Roman Szalla <roman@szalla.pl>
2012-11-01 14:47:55 +0100

    insert all files from directory on per-line basis

--- src/main/java/hbase2/hbase2/InsertLoadGenerator.java
+++ src/main/java/hbase2/hbase2/InsertLoadGenerator.java
@@ -1,6 +1,9 @@
 package hbase2.hbase2;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -26,71 +29,62 @@ public class InsertLoadGenerator {
 		Properties prop = System.getProperties();
 		System.out.println(prop.getProperty("java.class.path", null));
 
-		List<String> lines = new ArrayList<String>();
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-				InsertLoadGenerator.class.getResourceAsStream("/pg5000.txt")));
-		String line = br.readLine();
-		while (line != null) {
-			if ("".equals(line.trim())) {
-				line = br.readLine();
-				continue;
+		// all files in this directory will be stored in HBase
+		// on per line basics. It's key is hash of the line content.
+		// You can use text ebooks from http://www.gutenberg.org/ 
+		String textFilesPath = "/home/rszalla/git/hbase-helloworld/teksty";
+
+		String file;
+		File folder = new File(textFilesPath);
+		File[] listOfFiles = folder.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				file = listOfFiles[i].getAbsolutePath();
+				System.out.println("Processing file " + file);
+
+				FileInputStream fstream = new FileInputStream(file);
+				DataInputStream in = new DataInputStream(fstream);
+				BufferedReader br = new BufferedReader(
+						new InputStreamReader(in));
+
+				List<String> lines = new ArrayList<String>();
+
+				String line = br.readLine();
+				while (line != null) {
+					if ("".equals(line.trim())) {
+						line = br.readLine();
+						continue;
+					}
+					lines.add(line);
+					line = br.readLine();
+//					System.out.println(line);
+				}
+
+				Configuration config = HBaseConfiguration.create();
+
+				HTable table = new HTable(config, "myLittleHBaseTable");
+				long start, finish, diff;
+				String hash;
+
+				for (String currline : lines) {
+					hash = DigestUtils.shaHex(currline);
+
+					Put p = new Put(Bytes.toBytes("row-" + hash));
+
+					p.add(Bytes.toBytes("myLittleFamily"),
+							Bytes.toBytes("someQualifier"),
+							Bytes.toBytes(currline));
+
+					start = Calendar.getInstance().getTimeInMillis();
+					table.put(p);
+					finish = Calendar.getInstance().getTimeInMillis();
+					diff = finish - start;
+					System.out.println("Insert " + hash + " took " + diff
+							+ " ms");
+				}
+
 			}
-			lines.add(line);
-			line = br.readLine();
-			System.out.println(line);
-		}
-
-		// You need a configuration object to tell the client where to connect.
-		// When you create a HBaseConfiguration, it reads in whatever you've set
-		// into your hbase-site.xml and in hbase-default.xml, as long as these
-		// can be found on the CLASSPATH
-		Configuration config = HBaseConfiguration.create();
-
-		// This instantiates an HTable object that connects you to the
-		// "myLittleHBaseTable" table.
-		HTable table = new HTable(config, "myLittleHBaseTable");
-		long start, finish, diff;
-		String hash;
-
-		for (String currline : lines) {
-			hash = DigestUtils.shaHex(currline);
-
-			// To add to a row, use Put. A Put constructor takes the name of the
-			// row
-			// you want to insert into as a byte array. In HBase, the Bytes
-			// class
-			// has utility for converting all kinds of java types to byte
-			// arrays. In
-			// the below, we are converting the String "myLittleRow" into a byte
-			// array to use as a row key for our update. Once you have a Put
-			// instance, you can adorn it by setting the names of columns you
-			// want
-			// to update on the row, the timestamp to use in your update, etc.
-			// If no timestamp, the server applies current time to the edits.
-			Put p = new Put(Bytes.toBytes("row-" + hash));
-
-			// To set the value you'd like to update in the row 'myLittleRow',
-			// specify the column family, column qualifier, and value of the
-			// table
-			// cell you'd like to update. The column family must already exist
-			// in your table schema. The qualifier can be anything.
-			// All must be specified as byte arrays as hbase is all about byte
-			// arrays. Lets pretend the table 'myLittleHBaseTable' was created
-			// with a family 'myLittleFamily'.
-			p.add(Bytes.toBytes("myLittleFamily"),
-					Bytes.toBytes("someQualifier"), Bytes.toBytes(currline));
-
-			// Once you've adorned your Put instance with all the updates you
-			// want
-			// to make, to commit it do the following
-			// (The HTable#put method takes the Put instance you've been
-			// building
-			// and pushes the changes you made into hbase)
-			start = Calendar.getInstance().getTimeInMillis();
-			table.put(p);
-			finish = Calendar.getInstance().getTimeInMillis();
-			diff = finish - start;
-			System.out.println("Insert " + hash + " took " + diff + " ms");
 		}
 
 	}
